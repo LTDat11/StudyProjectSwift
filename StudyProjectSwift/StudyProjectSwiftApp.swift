e2555a950848c6b10ad5eb919c396d42d03780e8
Lư Tấn Đạt <95582749+LTDat11@users.noreply.github.com>
2026-01-03 12:05:27 +0700

    Initial Commit

---
+++ StudyProjectSwift/StudyProjectSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyProjectSwiftApp.swift
+//  StudyProjectSwift
+//
+//  Created by Lư Tấn Đạt on 3/1/26.
+//
+
+import SwiftUI
+
+@main
+struct StudyProjectSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
